3da317151554c98c03f9786e3f692ca4ca20d4c8
jevii_jay <99161820+jevaunD@users.noreply.github.com>
2025-10-27 16:45:04 -0500

    Delete Malicious_AutoFill_Testing directory

--- Malicious_AutoFill_Testing/popup.js
+++
@@ -1,9 +0,0 @@
-//This code is to be used in content.js to store the ids/names of the input fields and match them with names and ids of
-//credentials that would be very concerning to see get extracted from the background!
-// const inputs = document.querySelectorAll("input");
-//
-// inputs.forEach(input => {
-//     if (input.id == "email") {
-//         console.log("It matched!");
-//     }
-// });
